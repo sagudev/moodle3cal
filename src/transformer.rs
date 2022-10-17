d49b258c9be205cf5de1254ca6fd7c5cf00b9fde
sagudev <16504129+sagudev@users.noreply.github.com>
2022-10-17 06:12:46 +0200

    wasm

--- src/transformer.rs
+++ src/transformer.rs
@@ -1,3 +1,9 @@
 pub fn transform(s: String) -> String {
     s
 }
+
+#[test]
+fn test_file() {
+    let s = std::fs::read_to_string("./private/file.ics").unwrap();
+    println!("{}", transform(s));
+}
